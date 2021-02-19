364df2fa6d529c91a5af53df3e47a64fe1f87964
Nilesh Acharya <nileshacharya9047@gmail.com>
2021-02-19 12:22:44 +0530

    App dev

---
+++ Index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const host = "192.168.1.208";
+const site = "http://192.168.1.208:3000/home"
+
+
+
+
+app.get('/', (req, res) => {
+  res.send(`Hii please route to ${site}`)
+})
+
+app.get('/home', (req, res) => {
+    res.send('Hii Good Morning!! How may I help you?')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://${host}:${port}`)
+})
+
+
+
